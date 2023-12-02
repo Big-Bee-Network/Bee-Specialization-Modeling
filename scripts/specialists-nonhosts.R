09acc1b1280e430a3a000f1c3ac27bffec8d4333
cmsmith91 <colleensmith91@gmail.com>
2023-12-01 16:14:39 -0800

    adding script

--- scripts/specialists-nonhosts.R
+++ scripts/specialists-nonhosts.R
@@ -10,24 +10,30 @@ library(vroom)
 globi = vroom("modeling_data/globi_allNamesUpdated.csv") %>%
   mutate(bee_genus = sub(' .*',"",scientificName))
 
-# load the fowler data
-fowler_formatted = read_csv('modeling_data/fowler_formatted-7march2023.csv')
-diet_breadth = read_csv('modeling_data/bee_diet_breadth-7march2023.csv')
+# load the host data
+fowler_formatted = read_csv('modeling_data/fowler_formatted-30nov2023.csv') %>% 
+  mutate(ref='fowler')
+russell_formatted = read_csv('modeling_data/russell_formatted-30nov2023.csv')%>% 
+  mutate(ref='russell')
+hosts = fowler_formatted %>% bind_rows(russell_formatted)
 
-specialists = diet_breadth[diet_breadth$diet_breadth=='specialist',]$scientificName
-globi_u=globi %>% left_join(diet_breadth %>% distinct(scientificName,diet_breadth)) %>%
-  mutate(diet_breadth = ifelse(is.na(diet_breadth),'generalist',diet_breadth))
+# double check no bees are duplicated
+(dupes = hosts[duplicated(hosts$scientificName),]$scientificName)
 
-#double check no increase in the number of rows
-nrow(globi) ==nrow(globi_u)
+## both data.frames just contain specialitss. filter th globi data to be just that
+globi_u=globi %>% filter(scientificName %in% hosts$scientificName)
 
 
-# for bees on the Jarrod Fowler lists, what % visit their host plants?
+# for specialist bees, what % visit their host plants?
 # for the globi data: categorize interaction partner as host or non-host
 # loop through the bee speices in globi_u
 # get their host plants
 # if the interaciton partner is in the family or genus then categorize it as a host
 #fowler_specialists = #fowler[fowler$diet_breadth == 'specialist',]$scientificName
+
+n_distinct(globi_u$scientificName)
+globi_u %>% distinct(scientificName) %>% nrow(.)
+
 
 # let's exclude bees with fewer than 20 interactions in the globi dataset
 n_threshold = 20
@@ -36,20 +42,24 @@ bigN_bees20=globi_u %>%
   summarize(n=n()) %>% 
   filter(n>=n_threshold)
 
+#get sample sizes:
+n_distinct(bigN_bees20$scientificName)
+sum(bigN_bees20$n)
+
+with(bigN_bees20, paste0('there are ', n_distinct(scientificName), ' species with ', sum(n), ' records'))
+
 globi_summ_NoSizeFilter = globi_u %>% 
   group_by(scientificName,bee_family,plant_genus,plant_family) %>% 
   summarize(n_genus=n(),n_citations=n_distinct(sourceCitation)) 
 
 globi_specs = globi_summ_NoSizeFilter %>% 
-  filter(scientificName %in% bigN_bees20$scientificName) %>%
-  left_join(diet_breadth) %>%
-  filter(diet_breadth=='specialist') %>%
-  #filter(n_genus >=5) %>% ##let's exclude interactions with fewer than 5 records
-  mutate(bee_plant = paste(scientificName,plant_genus))
+  filter(scientificName %in% bigN_bees20$scientificName) 
+ # mutate(bee_plant = paste(scientificName,plant_genus))
+
 
 # double check that specialist bees have either family as host rank or genus
 # but not both
-which(fowler_formatted %>% split(.$scientificName) %>%
+which(hosts %>% split(.$scientificName) %>%
         purrr::map_lgl(function(df) n_distinct(df$host_rank)>1))
 
 my_index = which(globi_specs$scientificName  == 'Dufourea virgata')
@@ -59,13 +69,12 @@ empty_list = c()
 globi_host = globi_specs %>% split(1:nrow(globi_specs)) %>% 
   purrr::map_dfr(function(row){
     
-    (host_plant_df = fowler_formatted %>% 
+    (host_plant_df = hosts %>% 
        filter(scientificName == row$scientificName[1]))
     
     if(unique(host_plant_df$host_rank) =='genus') visiting_host = row$plant_genus  %in% host_plant_df$host
     if(unique(host_plant_df$host_rank) =='family') visiting_host = row$plant_family %in% host_plant_df$host
-    if(unique(host_plant_df$host_rank) =='tribe') visiting_host = row$plant_family =='Asteraceae'
-    
+
     return(row %>% mutate(host=visiting_host))
     
   })   #add 0 for bees that haven't visited their host plant
@@ -86,12 +95,17 @@ cex_lab = 1.5
 cex_axis=1.3
 cex_main = 1.5
 
-# pdf('figures/histogram_visits_hosts_specialists.pdf')
+#change y-axis to be percentages for histogram:
+h= hist(host_fidelity$sum_n*100, plot= F)
+h$density = h$counts/sum(h$counts)*100
+plot(h,freq=FALSE)
+
+pdf('figures/histogram_visits_hosts_specialists.pdf')
 par(mfrow=c(1,1), mar=c(5,5,4,1))
-hist(host_fidelity$sum_n*100,main='Pollen specialists - visits to host plants',
-     xlab='% of visits to host plants',col=point_col,cex.lab=cex_lab, 
+plot(h, freq=F, main='Pollen specialists - visits to host plants', ylab = "% of pollen specialist species",
+     xlab='% of visits to host plants',col=point_col,cex.lab=cex_lab, ylim = c(0,40),
      cex.axis=cex_axis, cex.main = cex_main)
-# dev.off()
+ dev.off()
 
 paste0('specialist bees visited their host plants on average ',round(mean(host_fidelity$sum_n*100),2),
        '% of the time (median = ',round(median(host_fidelity$sum_n*100),2),'%)')
@@ -104,11 +118,11 @@ host_fidelity %>% arrange(sum_n)
 paste('For',sum(host_fidelity$sum_n==0),'specialist bee species, there were no records of the bee visiting their pollen hosts')
 paste('these bees were:')
 (unfaithful_bees = host_fidelity[host_fidelity$sum_n==0,]$scientificName)
-fowler_formatted %>% filter(scientificName %in% unfaithful_bees) %>% 
+hosts %>% filter(scientificName %in% unfaithful_bees) %>% 
   distinct(scientificName, host,host_rank)
 
 globi_host %>% filter(scientificName == "Perdita zebrata")
-fowler_formatted %>% filter(scientificName == 'Perdita zebrata') %>% distinct(host)
+hosts %>% filter(scientificName == 'Perdita zebrata') %>% distinct(host)
 
 hist(host_fidelity$sum_cits*100)
 host_fidelity %>% filter(sum_n<.3)
@@ -119,7 +133,7 @@ with(host_fidelity,plot(sample_size,sum_n, xlab = "Sample size", ylab = "% of vi
                         cex.lab=cex_lab, cex.axis = cex_axis, pch = 16, col = adjustcolor('cadetblue',.5)))
 
 #what are low host fidelity bees with large sample sizes?
-host_fidelity %>% filter(sample_size >200 & sum_n<0.5)
+host_fidelity %>% filter(sample_size >200 & sum_n<0.5) %>% arrange(desc(sample_size))
 
 
 ## what about male vs female?
@@ -154,21 +168,21 @@ globi_summ_bySex = globi_u %>%
 
 
 globi_specs_sexed = globi_summ_bySex %>% 
-  left_join(diet_breadth) %>%
-  filter(diet_breadth=='specialist') %>%
   mutate(bee_plant = paste(scientificName,plant_genus)) %>%
-  filter(scientificName %in% bigN_bees20$scientificName) #filter to just bee bees above our threshold sample szie
-
+  filter(scientificName %in% bigN_bees20$scientificName) #%>% #filter to just bee bees above our threshold sample szie
+  #left_join(hosts)
+nrow(globi_specs_sexed)==nrow(globi_summ_bySex %>% filter(scientificName %in% bigN_bees20$scientificName))
+a= globi_specs_sexed %>% split(1:nrow(globi_specs_sexed))
+row=a[[198]]
 globi_host_bySex = globi_specs_sexed %>% split(1:nrow(globi_specs_sexed)) %>% 
   purrr::map_dfr(function(row){
     
-    (host_plant_df = fowler_formatted %>% 
+    (host_plant_df = hosts %>% 
        filter(scientificName == row$scientificName[1]))
     
     if(unique(host_plant_df$host_rank) =='genus') visiting_host = row$plant_genus  %in% host_plant_df$host
     if(unique(host_plant_df$host_rank) =='family') visiting_host = row$plant_family %in% host_plant_df$host
-    if(unique(host_plant_df$host_rank) =='tribe') visiting_host = row$plant_family =='Asteraceae'
-    
+
     return(row %>% mutate(host=visiting_host))
     
   }) 
@@ -182,8 +196,7 @@ hosts_by_sex1 = globi_host_bySex %>%
 
 # we'll only include bees with at least 10 observations for both male and feamles
 hosts_by_sex = hosts_by_sex1
-hosts_by_sex1 %>% split(.$scientificName) %>%
-  map_lgl(function(df) df)
+
 #first just plot as box plots
 with(hosts_by_sex %>% filter(sex != 'unknown'),boxplot(sum_n~sex))
 
@@ -217,7 +230,7 @@ loc_gen=1.2; loc_spec=1.8
 box_col = adjustcolor('white',0)
 
 
-# pdf('figures/males_v_females.pdf')
+ pdf('figures/males_v_females.pdf')
 par(mfrow=c(1,1))
 with(hosts_by_sex_bigN,stripchart(sum_n~sex,
                                   vertical=T,col=point_col,at=c(loc_gen,loc_spec),pch=16,
@@ -225,13 +238,16 @@ with(hosts_by_sex_bigN,stripchart(sum_n~sex,
                                   xlab = 'Bee sex',ylab = '% of visits to host plant'))
 for(i in 1:nrow(sexed_wide)){segments(loc_gen, sexed_wide$female[i], loc_spec, sexed_wide$male[i],lty=2,col=adjustcolor('black',.3))}
 with(hosts_by_sex_bigN,boxplot(sum_n~sex,col=box_col,add=T,at=c(loc_gen,loc_spec), axes = F,boxwex=c(.35,.35)))
-# dev.off()
+ dev.off()
+
+#get sample sizes
+paste0('there are ', n_distinct(hosts_by_sex_bigN$scientificName), ' species with ', sum(hosts_by_sex_bigN$sample_size), ' total records')
 
 
 
 
 #combine the two post-hoc figs together
-pdf('figures/host_fidelity_posthoc.pdf', width = 12)
+ pdf('figures/host_fidelity_posthoc-1dec2023.pdf', width = 12)
 par(mfrow=c(1,2), mar =c(4.5,4.5,4.2, 1))
 #is host fidelity a sampling artificat?
 with(host_fidelity,plot(log10(sample_size),sum_n*100, xlab = expression("Log"[10]*"(Sample size)"), ylab = "% of visits to host plants", 
@@ -243,7 +259,7 @@ with(hosts_by_sex_bigN,stripchart(sum_n*100~sex,
 for(i in 1:nrow(sexed_wide)){segments(loc_gen, sexed_wide$female[i]*100, loc_spec, sexed_wide$male[i]*100,lty=2,col=adjustcolor('black',.3))}
 with(hosts_by_sex_bigN,boxplot(sum_n*100~sex,col=box_col,add=T,at=c(loc_gen,loc_spec), axes = F,boxwex=c(.35,.35)))
 
-dev.off()
+ dev.off()
 
 
 
@@ -267,13 +283,3 @@ median(diff)
 
 
 
-
-
-
-####
-#what about nectarless plants
-plants_nectar <- read_excel("modeling_data/plants_nectar.xlsx") %>%
-  filter(nectar_status != 'unknown' & !is.na(nectar_status))
-
-host_fidelity %>% 
-  filter(host %in% plants_nectar)

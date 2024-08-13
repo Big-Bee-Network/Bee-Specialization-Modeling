816e590d362c9146dc6d1df5455d35905d3e358a
seltmann <enicospilus@gmail.com>
2024-08-12 17:45:37 -0700

    revision updates

--- scripts/bee_name_updates_Henriquez_Piskulich.R
+++ scripts/bee_name_updates_Henriquez_Piskulich.R
@@ -8,7 +8,10 @@ library(tidyverse)
 #Micralictoides treated as subgenus of Duforea
 #Dufourea subg. Micralictoides Timberlake, 1939. Ent. Soc. Amer., Ann. 32: 397.
 
-globi_allNamesUpdated <- read_csv("modeling_data/globi_allNamesUpdated.csv")
+#globi_allNamesUpdated <- read_csv("modeling_data/globi_allNamesUpdated.csv")
+chesshire2023_beeArea11april2023 <- read_csv("modeling_data/chesshire2023_beeArea-11april2023.csv")
+#globi_phyloDiv <- read_csv("modeling_data/globi_phyloDiv.csv")
+DATA_beetaxaknownhostsallplanttaxa_11272023 <- read_csv("modeling_data/DATA_bee-taxa-known-hosts_all-plant-taxa_11-27-2023.csv")
 
 
 # Perform multiple generic replacements
@@ -26,6 +29,21 @@ replace_species <- function(x) {
 globi_allNamesUpdated <- globi_allNamesUpdated %>%
   mutate_all(~ replace_species(.))
 
+# Apply the replacement function to all columns in the dataframe
+chesshire2023_beeArea11april2023 <- chesshire2023_beeArea11april2023 %>%
+  mutate_all(~ replace_species(.))
+
+# Apply the replacement function to all columns in the dataframe
+globi_phyloDiv <- globi_phyloDiv %>%
+  mutate_all(~ replace_species(.))
+
+# Apply the replacement function to all columns in the dataframe
+DATA_beetaxaknownhostsallplanttaxa_11272023 <- DATA_beetaxaknownhostsallplanttaxa_11272023 %>%
+  mutate_all(~ replace_species(.))
+
 
 #write updated dataframe
 #write_csv(globi_allNamesUpdated,'modeling_data/globi_allNamesUpdated_Henriquez_Piskulich.csv')
+write_csv(chesshire2023_beeArea11april2023,'modeling_data/chesshire2023_beeArea11april2023_revision.csv')
+write_csv(globi_phyloDiv,'modeling_data/globi_phyloDiv_revision.csv')
+write_csv(DATA_beetaxaknownhostsallplanttaxa_11272023,'modeling_data/DATA_bee-taxa-known-hosts_all-plant-taxa_11-27-2023_revision.csv')

8367e2419f588ee652199217e28b9c3938338f22
seltmann <enicospilus@gmail.com>
2024-08-16 17:00:35 -0700

    name script updates

--- scripts/bee_name_updates_Henriquez_Piskulich.R
+++ scripts/bee_name_updates_Henriquez_Piskulich.R
@@ -8,20 +8,21 @@ library(tidyverse)
 #Micralictoides treated as subgenus of Duforea
 #Dufourea subg. Micralictoides Timberlake, 1939. Ent. Soc. Amer., Ann. 32: 397.
 
-#globi_allNamesUpdated <- read_csv("modeling_data/globi_allNamesUpdated.csv")
-chesshire2023_beeArea11april2023 <- read_csv("modeling_data/chesshire2023_beeArea-11april2023.csv")
-#globi_phyloDiv <- read_csv("modeling_data/globi_phyloDiv.csv")
-DATA_beetaxaknownhostsallplanttaxa_11272023 <- read_csv("modeling_data/DATA_bee-taxa-known-hosts_all-plant-taxa_11-27-2023.csv")
+globi_allNamesUpdated <- read_csv('modeling_data/globi_allNamesUpdated_Henriquez_Piskulich.csv')
+
+russell_formatted <- read_csv("final_data/other_data_files/russell_formatted-30nov2023.csv")
+
+fowler_formatted <- read_csv("final_data/other_data_files/fowler_formatted-30nov2023.csv")
 
 
 # Perform multiple generic replacements
 replace_species <- function(x) {
-  x <- gsub("Pseudopanurgus", "Protandrena", x)
-  x <- gsub("Peponapis", "Xenoglossa", x)
-  x <- gsub("Tetraloniella", "Xenoglossa", x)
-  x <- gsub("Syntrichalonia", "Xenoglossa", x)
-  x <- gsub("Cemolobus", "Xenoglossa", x)
-  x <- gsub("Micralictoides", "Dufourea", x)
+  x <- gsub("Pseudopanurgus", "Protandrena", x, ignore.case = TRUE)
+  x <- gsub("Peponapis", "Xenoglossa", x, ignore.case = TRUE)
+  x <- gsub("Tetraloniella", "Xenoglossa", x, ignore.case = TRUE)
+  x <- gsub("Syntrichalonia", "Xenoglossa", x, ignore.case = TRUE)
+  x <- gsub("Cemolobus", "Xenoglossa", x, ignore.case = TRUE)
+  x <- gsub("Micralictoides", "Dufourea", x, ignore.case = TRUE)
   return(x)
 }
 
@@ -30,20 +31,19 @@ globi_allNamesUpdated <- globi_allNamesUpdated %>%
   mutate_all(~ replace_species(.))
 
 # Apply the replacement function to all columns in the dataframe
-chesshire2023_beeArea11april2023 <- chesshire2023_beeArea11april2023 %>%
+fowler_formatted <- fowler_formatted %>%
   mutate_all(~ replace_species(.))
 
 # Apply the replacement function to all columns in the dataframe
-globi_phyloDiv <- globi_phyloDiv %>%
+russell_formatted <- russell_formatted %>%
   mutate_all(~ replace_species(.))
 
-# Apply the replacement function to all columns in the dataframe
-DATA_beetaxaknownhostsallplanttaxa_11272023 <- DATA_beetaxaknownhostsallplanttaxa_11272023 %>%
-  mutate_all(~ replace_species(.))
 
 
 #write updated dataframe
-#write_csv(globi_allNamesUpdated,'modeling_data/globi_allNamesUpdated_Henriquez_Piskulich.csv')
-write_csv(chesshire2023_beeArea11april2023,'modeling_data/chesshire2023_beeArea11april2023_revision.csv')
-write_csv(globi_phyloDiv,'modeling_data/globi_phyloDiv_revision.csv')
-write_csv(DATA_beetaxaknownhostsallplanttaxa_11272023,'modeling_data/DATA_bee-taxa-known-hosts_all-plant-taxa_11-27-2023_revision.csv')
+write_csv(globi_allNamesUpdated,'modeling_data/globi_allNamesUpdated_Henriquez_Piskulich.csv')
+
+write_csv(fowler_formatted,'modeling_data/fowler_formatted-15Aug2024.csv')
+
+write_csv(russell_formatted,'modeling_data/russell_formatted-15Aug2024.csv')
+
